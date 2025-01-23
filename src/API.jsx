753d339fd217d911e5001cce0ae6aa5ec0748df7
Rahiman Shaik <rahimanshaik13@gmail.com>
2025-01-23 20:30:32 +0530

    fifth

--- src/API.jsx
+++ src/API.jsx
@@ -1,4 +1,5 @@
-const Api_url='http://localhost:5000'
+// const Api_url='http://localhost:5000'
+const Api_url='https://projexino-backend.onrender.com'
 // const Api_url='http://localhost:1337'
 
 export default Api_url
